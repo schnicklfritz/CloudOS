6917e9df14e64a37c04de88d5fe9353d953ba0bf
schnicklfritz <63166008+schnicklfritz@users.noreply.github.com>
2026-02-22 06:15:39 -0500

    Complete supervisord.conf + entrypoint.sh: remove early XFCE/DBus, add ssh-keygen/password auth, pulseaudio --start tcp 8081, kasmvncserver :1 explicit path; fixes exit 13/255/1/2 cascade

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 set -e
 
+# GPU check
 nvidia-smi || true
 
-chown -R fritz:fritz /home/fritz/.vnc /etc/kasmvnc
+# Perms + config init
+chown -R fritz:fritz /home/fritz/.vnc /etc/kasmvnc /usr/share/novnc
 [ ! -f /etc/kasmvnc/kasmvnc.yaml ] && cp /defaults/kasmvnc.yaml /etc/kasmvnc/ && chown fritz:fritz /etc/kasmvnc/kasmvnc.yaml
 
-# PulseAudio config
-su - fritz -c "mkdir -p ~/.config/pulse && echo 'load-module module-detect' > ~/.config/pulse/default.pa"
-
-# DBus + XFCE pre-launch
-export XDG_RUNTIME_DIR=/run/user/$(id -u fritz)
-mkdir -p $XDG_RUNTIME_DIR
-chown fritz:fritz $XDG_RUNTIME_DIR
-su - fritz -c "dbus-daemon --session --fork --print-address > ~/.dbus-session && export $(cat ~/.dbus-session) && xfce4-session &" || true
-sleep 5
+# SSH keys (unblocks sshd 255)
+mkdir -p /var/run/sshd && \
+ssh-keygen -A && \
+sed -i 's/#*PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config && \
+sed -i 's/#*PasswordAuthentication.*/PasswordAuthentication yes/' /etc/ssh/sshd_config
 
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
